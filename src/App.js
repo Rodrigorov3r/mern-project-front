60113b1a0cb3daf8743be65977b1582e4f7c773a
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-09-12 21:39:05 -0300

    componente crear

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Mostrar from './blogs/Mostrar';
+import Crear from './blogs/Crear';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Mostrar />} />
+          <Route path="/crear" element={<Crear />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/blogs/Crear.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import React, { useEffect } from 'react';
+
+const Crear = () => {
+  return <div>Crear</div>;
+};
+
+export default Crear;
