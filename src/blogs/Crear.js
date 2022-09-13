0c28ce5bad5e43d8e78e86be28dfe950aa8a95c7
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-09-12 22:52:37 -0300

    post 1.0

--- src/blogs/Crear.js
+++ src/blogs/Crear.js
@@ -3,10 +3,12 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Crear = () => {
-  const [data, setData] = useState({
-    title: '',
-    content: '',
-  });
+  //   const [data, setData] = useState({
+  //     title: '',
+  //     content: '',
+  //   });
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
   const back = useNavigate();
 
   const URL = 'http://localhost:8000/blogs';
@@ -14,10 +16,48 @@ const Crear = () => {
   //post data
   const store = async (e) => {
     e.preventDefault();
-    axios.post(URL + '/crear', { title: data.title, content: data.content });
+    axios.post(URL, { title: title, content: content });
     back('/');
   };
-  return <h1>Crear</h1>;
+
+  //   const handleInput = (e) => {
+  //     const { name, value } = e.target;
+
+  //     setData({
+  //       ...data,
+  //       [name]: value,
+  //     });
+  //     console.log(value);
+  //   };
+
+  return (
+    <div>
+      <h2>Crear Post</h2>
+      <form onSubmit={store}>
+        <div className="mb-3">
+          <label className="form-label">Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Content</label>
+          <textarea
+            type="text"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Crear
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Crear;
